db03df8df1aa88acedb13bd441175d76584be9f2
Keila Lopez <keilalofra@gmail.com>
2022-09-27 12:36:12 -0700

    commit tree cf2b802f56a2cd81c5b0abbbb1dda3015ac26d2e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        let currentName = names[i]
+        let message = `Thank you, ${currentName} for the wonderful ${event} gift!`
+        messages.push(message);
+    }
+    return messages;
+}
+
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+
+function countDown(number) {
+    let counter = number
+    while (counter > 0) {
+        console.log(counter)
+        counter--
+    }
+    console.log(0)
+}
+
+countDown(5)
